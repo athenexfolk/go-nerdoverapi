e0725833b41c86521c28f6954538fd9e9b7c32fd
athenexfolk <folkadpocket99@gmail.com>
2025-04-29 15:03:01 +0700

    fixed no return after err

--- internal/category/handler/handler.go
+++ internal/category/handler/handler.go
@@ -12,6 +12,7 @@ func GetAllCategoriesHandler(c *gin.Context) {
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
 	}
 
 	c.JSON(http.StatusOK, categoryList)
